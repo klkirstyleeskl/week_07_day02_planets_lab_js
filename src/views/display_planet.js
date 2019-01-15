5f8b178a7f69933504f9d82b287dc92fcd31adfa
klkirstyleeskl <k.lees@btinternet.com>
2019-01-15 16:07:08 +0000

    adds stylies

--- src/views/display_planet.js
+++ src/views/display_planet.js
@@ -19,12 +19,13 @@ DisplayPlanet.prototype.renderInfo = function (planet) {
   planetParagraph.textContent = (`This is ${planet.name}. It's orbit is ${planet.orbit}
   A day on ${planet.name} is approximately ${planet.day} Earth days. It is
   ${planet.surfaceArea} times the size of Earth. ${planet.name} has a volume of ${planet.volume}
-  compared to Earth. Its gravitational pull is ${planet.gravity} of Earth and it
-  has ${planet.moons}. Here is a photo wot I took on my holidays:`)
+  compared to Earth. Its gravitational pull is ${planet.gravity} times that of Earth and it
+  has ${planet.moons} moons. Here is a photo wot I took on my holidays:`)
   const planetPhoto = document.createElement('img');
-  console.dir(planetPhoto);
-  planetPhoto.src = planet.image;
 
+  planetPhoto.src = planet.image;
+  planetPhoto.className = 'planet-photo';
+console.dir(planetPhoto);
   resultSection.appendChild(planetParagraph);
   resultSection.appendChild(planetPhoto);
 };
