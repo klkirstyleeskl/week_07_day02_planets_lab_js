079973e6e93a05cf31a652554b47f383406fd6e0
klkirstyleeskl <k.lees@btinternet.com>
2019-01-15 14:49:13 +0000

    mercury button clickable

--- src/views/nav_view.js
+++ src/views/nav_view.js
@@ -6,16 +6,26 @@ const NavView = function () {
 
 NavView.prototype.bindEvents = function () {
 const mercuryButton = document.querySelector('#Mercury');
-const venusButton = document.querySelector('#Venus');
-const earthButton = document.querySelector('#Earth');
-const marsButton = document.querySelector('#Mars');
-const jupiterButton = document.querySelector('#Jupiter');
-const saturnButton = document.querySelector('#Saturn');
-const uranusButton = document.querySelector('#Uranus');
-console.log(mercuryButton);
-  menu.addEventListener('click', (event) => {
-      console.log('testing');
+mercuryButton.addEventListener('click', (event) => {
+  console.log('I clicked Mercury');
+  PubSub.publish('NavView:button-clicked', 0);
 });
+
+const venusButton = document.querySelector('#Venus');
+
+const earthButton = document.querySelector('#Earth');
+
+const marsButton = document.querySelector('#Mars');
+
+const jupiterButton = document.querySelector('#Jupiter');
+
+const saturnButton = document.querySelector('#Saturn');
+
+const uranusButton = document.querySelector('#Uranus');
+
+const neptuneButton = document.querySelector('#Neptune');
+console.log(mercuryButton);
+
 
 
 };
